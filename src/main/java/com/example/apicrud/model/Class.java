c0db82988aec914bcaa4a7588705b3114c9f1b66
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-08-13 00:20:39 +0700

    baitap2

--- src/main/java/com/example/apicrud/model/Class.java
+++ src/main/java/com/example/apicrud/model/Class.java
@@ -1,6 +1,8 @@
 package com.example.apicrud.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
@@ -12,8 +14,12 @@ public class Class {
     private int id;
     @Basic
     @Column(name = "name")
+    @NotNull
+    @Size(min = 5,message = "Điền đúng tên vào")
     private String name;
     @Basic
+    @NotNull
+    @Size(min = 5,message = "Điền đúng tên lớp vào")
     @Column(name = "room")
     private String room;
     @Basic
--- src/main/java/com/example/apicrud/service/ClassServiceimpl.java
+++ src/main/java/com/example/apicrud/service/ClassServiceimpl.java
@@ -13,7 +13,8 @@ public class ClassServiceimpl implements ClassService {
     ClassRepository classRepository;
 
     @Override
-    public void saveClass(Class c) {classRepository.save(c);
+    public void saveClass(Class c) {
+        classRepository.save(c);
     }
 
     @Override
